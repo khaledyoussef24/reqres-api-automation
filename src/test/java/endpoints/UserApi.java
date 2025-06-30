40cb4a741c21d4bea57d686065806556691ef3d4
khaledyoussef24 <mahmoudk@incubaid.com>
2025-06-30 08:34:44 +0300

    fix: use RestAssured.basePath=/api & simplify endpoints

--- src/test/java/endpoints/UserApi.java
+++ src/test/java/endpoints/UserApi.java
@@ -5,32 +5,33 @@ import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 public class UserApi {
+  public static Response createUser(String name, String job) {
+    return given()
+        .contentType("application/json")
+        .accept("application/json")
+        .body(Map.of("name", name, "job", job))
+      .when()
+        .post("/users");
+  }
 
-    public static Response createUser(String name, String job) {
-        return given()
-                .contentType("application/json")
-                .body(Map.of("name", name, "job", job))
-            .when()
-                .post("/api/users");
-    }
+  public static Response updateUser(String id, String name, String job) {
+    return given()
+        .contentType("application/json")
+        .accept("application/json")
+        .body(Map.of("name", name, "job", job))
+      .when()
+        .put("/users/" + id);
+  }
 
-    public static Response updateUser(String id, String name, String job) {
-        return given()
-                .contentType("application/json")
-                .body(Map.of("name", name, "job", job))
-            .when()
-                .put("/api/users/" + id);
-    }
+  public static Response getUser(String id) {
+    return given()
+      .when()
+        .get("/users/" + id);
+  }
 
-    public static Response getUser(String id) {
-        return given()
-            .when()
-                .get("/api/users/" + id);
-    }
-
-    public static Response deleteUser(String id) {
-        return given()
-            .when()
-                .delete("/api/users/" + id);
-    }
+  public static Response deleteUser(String id) {
+    return given()
+      .when()
+        .delete("/users/" + id);
+  }
 }
